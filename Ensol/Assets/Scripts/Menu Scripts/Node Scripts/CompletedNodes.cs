5ff3dada9def169fe30924da1195adcd9db78fe5
Jsatriano <72236388+Jsatriano@users.noreply.github.com>
2023-02-21 16:13:36 -0800

    added lots of map stuff

--- Ensol/Assets/Scripts/Menu Scripts/Node Scripts/CompletedNodes.cs
+++ Ensol/Assets/Scripts/Menu Scripts/Node Scripts/CompletedNodes.cs
@@ -9,10 +9,27 @@ public class CompletedNodes : MonoBehaviour
                 bearNode, brokenMachineNode, securityTowerNode, birdNode,
                 powerGridNode, metalFieldNode, computerNode = false;
 
+    public Button computerNodeButton;
+
     public GameObject[] mapButtons;
-    public Button[] editMapButtons;
-    
-    
+    public GameObject[] mapScenery;
+    public GameObject[] undiscovered;
+
+    /* 
+    ------  KEY  ------
+    mapButtons[0] = cabin
+    mapButtons[1] = deer
+    mapButtons[2] = river
+    mapButtons[3] = gate
+    mapButtons[4] = river control
+    mapButtons[5] = bear
+    mapButtons[6] = broken machine
+    mapButtons[7] = security tower
+    mapButtons[8] = bird
+    mapButtons[9] = power grid
+    mapButtons[10] = metal field
+    mapButtons[11] = computer
+    */
 
     public void Start()
     {
@@ -24,50 +41,64 @@ public class CompletedNodes : MonoBehaviour
         if(cabinNode)
         {
             mapButtons[0].SetActive(true);
+            mapScenery[0].SetActive(true);
+            undiscovered[0].SetActive(false);
+
         }
         if(deerNode)
         {
-            mapButtons[1].SetActive(true);
+            undiscovered[0].SetActive(true);
+            //mapButtons[1].SetActive(true);
+            //mapScenery[1].SetActive(true);
         }
         if(riverNode)
         {
-            mapButtons[2].SetActive(true);
+            //mapButtons[2].SetActive(true);
+            //mapScenery[2].SetActive(true);
         }
         if(gateNode)
         {
-            mapButtons[3].SetActive(true);
+            //mapButtons[3].SetActive(true);
+            //mapScenery[3].SetActive(true);
         }
         if(riverControlNode)
         {
-            mapButtons[4].SetActive(true);
+            //mapButtons[4].SetActive(true);
+            //mapScenery[4].SetActive(true);
         }
         if(bearNode)
         {
-            mapButtons[5].SetActive(true);
+            //mapButtons[5].SetActive(true);
+            //mapScenery[5].SetActive(true);
         }
         if(brokenMachineNode)
         {
-            mapButtons[6].SetActive(true);
+            //mapButtons[6].SetActive(true);
+            //mapScenery[6].SetActive(true);
         }
         if(securityTowerNode)
         {
-            mapButtons[7].SetActive(true);
+            //mapButtons[7].SetActive(true);
+            //mapScenery[7].SetActive(true);
         }
         if(birdNode)
         {
-            mapButtons[8].SetActive(true);
+            //mapButtons[8].SetActive(true);
+            //mapScenery[8].SetActive(true);
         }
         if(powerGridNode)
         {
-            mapButtons[9].SetActive(true);
+            //mapButtons[9].SetActive(true);
+            //mapScenery[9].SetActive(true);
         }
         if(metalFieldNode)
         {
-            mapButtons[10].SetActive(true);
+            //mapButtons[10].SetActive(true);
+            //mapScenery[10].SetActive(true);
         }
         if(computerNode)
         {
-            editMapButtons[11].interactable = true;
+            computerNodeButton.interactable = true;
         }
     }
 }
