4d8142815b0d498e6701c4e9adfb2215e9cd34f6
Sriharsha-Madala <srmadala@ucsc.edu>
2023-02-23 15:33:29 -0800

    made message appear in console

--- Ensol/Assets/Scripts/Environment Scripts/Story Elements/Footprints.cs
+++ Ensol/Assets/Scripts/Environment Scripts/Story Elements/Footprints.cs
@@ -5,22 +5,11 @@ using UnityEngine;
 public class Footprints : MonoBehaviour
 {
     public Collider c;
-    public static GameObject First {get; private set;}
-    void Start()
-    {
-        if (First == null) {
-            First = GameObject.Find("First");
-        }
-        First.SetActive(false);
-    }
     // Update is called once per frame
     void Update()
     {
         if(c.enabled == false) {
-            //Instantiate(FloatingTextPrefab, transform.position, Quaternion.identity, transform); 
-            First.SetActive(true);
-            print("working");
-            //StartCoroutine(ShowMessage("Boot footprints are visible in the mud on the bank of the river. They lead directly into the water, then reappear coming out on the other side. The water is moving too fast for you to attempt to cross it.", 1f));
+            print("Boot footprints are visible in the mud on the bank of the river. They lead directly into the water, then reappear coming out on the other side. The water is moving too fast for you to attempt to cross it.");
         }
     }
 }
