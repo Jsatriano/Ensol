f7927a3560633d3055dddfd895ede5fb04aedde5
matty217 <mathill@ucsc.edu>
2023-03-01 11:47:13 -0800

    fixing starting node

--- Ensol/Assets/Scripts/Environment Scripts/Building Scripts/ToggleWalls.cs
+++ Ensol/Assets/Scripts/Environment Scripts/Building Scripts/ToggleWalls.cs
@@ -3,36 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class ToggleWalls : MonoBehaviour
 {
     // Matty
     // script to toggle the walls when walking inside/outside of a building
-    void Start()
-    {
-        var object : GameObject;
-    }
+
+    public static GameObject walls;
+
+    
     void OnTriggerEnter(Collider other) // Check if Player is inside
     {
-        FadeWallsOut();
+        walls.SetActive(false);
+        Debug.Log("walls off");
     }
 
     void OnTriggerExit(Collider other) // Check if Player is outside
     {
-        FadeWallsIn();
-    }
-
-    public FadeWallsOut() // function to slowly fade the walls out
-    {
-        object.SetActive(false);
-        Debug.Log("walls off");
-    }
-
-    public FadeWallsIn() // function to slowly fade the walls in
-    {
-        object.SetActive(true);
+        walls.SetActive(true);
         Debug.Log("walls on");
     }
+
 }
 
