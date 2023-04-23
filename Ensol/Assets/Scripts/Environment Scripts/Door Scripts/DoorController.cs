07dc6189f6ad92cd84062410fed893966874074c
elorenz1 <48806756+EmersenL@users.noreply.github.com>
2023-04-22 18:56:00 -0700

    fixed door sounds

--- Ensol/Assets/Scripts/Environment Scripts/Door Scripts/DoorController.cs
+++ Ensol/Assets/Scripts/Environment Scripts/Door Scripts/DoorController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     public GameObject door;
+    public int soundDoor;
     public float openRot, speed;
     private bool opened = false;
     [HideInInspector] public bool opening = false;
@@ -27,7 +28,14 @@ public class DoorController : MonoBehaviour
     {
         if (opened == false)
         {
-            AudioManager.instance.PlayOneShot(FMODEvents.instance.envFenceGateOpen, this.transform.position);
+            if (soundDoor == 1)
+            {
+                AudioManager.instance.PlayOneShot(FMODEvents.instance.envCabinDoorOpen, this.transform.position);
+            }
+            else if (soundDoor == 2)
+            {
+                AudioManager.instance.PlayOneShot(FMODEvents.instance.envFenceGateOpen, this.transform.position);
+            }
             opened = true;
         }
         opening = true;
