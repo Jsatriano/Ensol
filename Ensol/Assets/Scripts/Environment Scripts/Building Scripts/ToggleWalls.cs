053c8f33e15333e0ee5c1e1c08a1935d575c6df8
aplaxco1 <aplaxco@ucsc.edu>
2023-04-28 13:11:34 -0700

    camera zoom works now (no its not finished yet)

--- Ensol/Assets/Scripts/Environment Scripts/Building Scripts/ToggleWalls.cs
+++ Ensol/Assets/Scripts/Environment Scripts/Building Scripts/ToggleWalls.cs
@@ -3,24 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Cinemachine;
 
 public class ToggleWalls : MonoBehaviour
 {
     // Matty
     // script to toggle the walls when walking inside/outside of a building
 
+    // Autumn
+    // script Camera to Zoom in and out when walking inside/outside of a building
+
     public GameObject walls;
 
+    GameObject mainCam;
+    CinemachineBrain brain;
+    CinemachineVirtualCamera vcam;
+
+    void Awake () {
+        mainCam = GameObject.Find("MainCamera");
+        brain = mainCam.GetComponent<CinemachineBrain>();
+        vcam = brain.ActiveVirtualCamera as CinemachineVirtualCamera;
+    }
+ 
+    void Update()
+    {
+        vcam = brain.ActiveVirtualCamera as CinemachineVirtualCamera;
+    }
     
     void OnTriggerEnter(Collider other) // Check if Player is inside
     {
         walls.SetActive(false);
+        if (vcam) {
+            vcam.m_Lens.OrthographicSize = 4.66f;
+        }
         Debug.Log("walls off");
     }
 
     void OnTriggerExit(Collider other) // Check if Player is outside
     {
         walls.SetActive(true);
+        if (vcam) {
+            vcam.m_Lens.OrthographicSize = 6.66f;
+        }
         Debug.Log("walls on");
     }
 
