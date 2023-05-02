b42c0b3ecc4f14256a2e8bb8c9e38bf80a7a430e
aplaxco1 <aplaxco@ucsc.edu>
2023-05-02 16:04:17 -0700

    gettin a nice little camera zoom transition going

--- Ensol/Assets/Scripts/Environment Scripts/Building Scripts/ToggleWalls.cs
+++ Ensol/Assets/Scripts/Environment Scripts/Building Scripts/ToggleWalls.cs
@@ -19,6 +19,13 @@ public class ToggleWalls : MonoBehaviour
     CinemachineBrain brain;
     CinemachineVirtualCamera vcam;
 
+    private float zoomed_out = 6.88f;
+    private float increment = 0.02f;
+    private float zoomed_in = 4.88f;
+
+    private bool inside = false;
+    private bool outside = false;
+
     void Awake () {
         mainCam = GameObject.Find("MainCamera");
         brain = mainCam.GetComponent<CinemachineBrain>();
@@ -28,25 +35,33 @@ public class ToggleWalls : MonoBehaviour
     void Update()
     {
         vcam = brain.ActiveVirtualCamera as CinemachineVirtualCamera;
+
+        // if player walks inside, zoom in the camera
+        if (vcam && vcam.m_Lens.OrthographicSize <= zoomed_out && outside) { 
+            vcam.m_Lens.OrthographicSize = Mathf.MoveTowards(vcam.m_Lens.OrthographicSize, vcam.m_Lens.OrthographicSize + increment, 1f); 
+        }
+
+        // if player walks outside, zoom out the camera
+        if (vcam && vcam.m_Lens.OrthographicSize >= zoomed_in && inside) { 
+            vcam.m_Lens.OrthographicSize = Mathf.MoveTowards(vcam.m_Lens.OrthographicSize, vcam.m_Lens.OrthographicSize - increment, 1f); 
+        }
+
     }
     
     void OnTriggerEnter(Collider other) // Check if Player is inside
     {
         walls.SetActive(false);
-        if (vcam) {
-            vcam.m_Lens.OrthographicSize = 4.66f;
-        }
+        inside = true;
+        outside = false;
         Debug.Log("walls off");
     }
 
     void OnTriggerExit(Collider other) // Check if Player is outside
     {
         walls.SetActive(true);
-        if (vcam) {
-            vcam.m_Lens.OrthographicSize = 6.66f;
-        }
+        inside = false;
+        outside = true;
         Debug.Log("walls on");
     }
-
 }
 
