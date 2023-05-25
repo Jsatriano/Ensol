eaac07cc5c23a767efd43e3fb12e44e8e2590a0f
aplaxco1 <aplaxco@ucsc.edu>
2023-05-25 16:35:03 -0700

    adding rotation to plush movement and also it stops moving when dialog

--- Ensol/Assets/Scripts/PlushMovement.cs
+++ Ensol/Assets/Scripts/PlushMovement.cs
@@ -7,28 +7,37 @@ public class PlushMovement : MonoBehaviour
 
     [SerializeField] public List<Vector3> positions;
     [SerializeField] public GameObject plush;
+    [SerializeField] public float moveSpeed = 1.0f;
+    [SerializeField] public float rotateSpeed = 4.0f;
 
-    private int currPositionIndex = 1;
-    private float speed = 1.0f;
+
+    private int currPositionIndex = 0;
+    private Quaternion rotation;
+    private Vector3 moveDirection;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveDirection = (plush.transform.localPosition - positions[currPositionIndex]).normalized;
+        rotation = Quaternion.LookRotation(moveDirection);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (plush.transform.localPosition != positions[currPositionIndex]) {
-            var step =  speed * Time.deltaTime; // calculate distance to move
-            plush.transform.localPosition = Vector3.MoveTowards(plush.transform.localPosition, positions[currPositionIndex], step);
+        if (plush.transform.localPosition != positions[currPositionIndex] && !DialogueManager.GetInstance().dialogueisPlaying) {
+            // rotate plush towards next position
+            plush.transform.localRotation = Quaternion.Slerp(plush.transform.localRotation, rotation, rotateSpeed*Time.deltaTime);
+            // move plush towards next position
+            plush.transform.localPosition = Vector3.MoveTowards(plush.transform.localPosition, positions[currPositionIndex], moveSpeed*Time.deltaTime);
         }
         else {
-            currPositionIndex += 1;
-            if (currPositionIndex >= positions.Count) {
-                currPositionIndex = 0;
-            }
+            // update next position to move to
+            currPositionIndex = Random.Range(0, positions.Count);
+            // update the position to rotate towards
+            moveDirection = (plush.transform.localPosition - positions[currPositionIndex]).normalized;
+            rotation = Quaternion.LookRotation(moveDirection);
         }
         
     }
