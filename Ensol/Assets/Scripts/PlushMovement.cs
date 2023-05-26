dc3b0ae537fb4d40e86f4271478e939c9ca8c771
aplaxco1 <aplaxco@ucsc.edu>
2023-05-26 11:51:46 -0700

    adding some wait time between movement and also only starts moving after interacting with it at least once

--- Ensol/Assets/Scripts/PlushMovement.cs
+++ Ensol/Assets/Scripts/PlushMovement.cs
@@ -9,14 +9,15 @@ public class PlushMovement : MonoBehaviour
     [SerializeField] public GameObject plush;
     [SerializeField] public float moveSpeed = 1.0f;
     [SerializeField] public float rotateSpeed = 4.0f;
+    [SerializeField] public int maxWaitTime = 10;
 
 
     private int currPositionIndex = 0;
     private Quaternion rotation;
     private Vector3 moveDirection;
     private bool colliding = false;
-    private bool talkingToPlush = false;
-
+    private bool waiting = false;
+    private bool hasInteractedOnce = false;
 
     // Start is called before the first frame update
     void Start()
@@ -28,35 +29,42 @@ public class PlushMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // checks if player is in dialouge with plush
-        if (colliding && DialogueManager.GetInstance().dialogueisPlaying) {
-            talkingToPlush = true;
-        }
-        else {
-            talkingToPlush = false;
+        if (colliding && DialogueManager.GetInstance().dialogueisPlaying && !hasInteractedOnce) {
+            hasInteractedOnce = true;
         }
 
-        if (plush.transform.localPosition != positions[currPositionIndex] && !talkingToPlush) {
+        if (plush.transform.localPosition != positions[currPositionIndex] && !(colliding && DialogueManager.GetInstance().dialogueisPlaying) && !waiting && hasInteractedOnce) {
             // rotate plush towards next position
             plush.transform.localRotation = Quaternion.Slerp(plush.transform.localRotation, rotation, rotateSpeed*Time.deltaTime);
             // move plush towards next position
             plush.transform.localPosition = Vector3.MoveTowards(plush.transform.localPosition, positions[currPositionIndex], moveSpeed*Time.deltaTime);
         }
         else {
-            // update next position to move to
-            currPositionIndex = Random.Range(0, positions.Count);
-            // update the position to rotate towards
-            moveDirection = (plush.transform.localPosition - positions[currPositionIndex]).normalized;
-            rotation = Quaternion.LookRotation(moveDirection);
+            if (!waiting) {
+                // wait
+                StartCoroutine(PlushWait());
+                // update next position to move to
+                currPositionIndex = Random.Range(0, positions.Count);
+                // update the position to rotate towards
+                moveDirection = (plush.transform.localPosition - positions[currPositionIndex]).normalized;
+                rotation = Quaternion.LookRotation(moveDirection);
+            }
         }
         
+    }
+
+    IEnumerator PlushWait()
+    {
+        waiting = true;
+        var time = Random.Range(0, maxWaitTime);
+        yield return new WaitForSeconds(time);
+        waiting = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            Debug.Log("COLLIDING");
             colliding = true;
         }
     }
